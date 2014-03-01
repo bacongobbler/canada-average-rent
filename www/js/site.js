7d630d0a6f91374533a3e10cce6cba116576a1f5
Chocolancer <jasonbolanos92@gmail.com>
2014-03-01 12:41:47 -0800

    Colour-coded province. Creating clickable cities.

--- www/js/site.js
+++ www/js/site.js
@@ -9,12 +9,15 @@ $(function () {
     
     d3.json("data/canadamapprovinces.json", function(error, can) {
         var provinces = topojson.feature(can, can.objects.statelines),
+            places = topojson.feature(can, can.objects.places),
             projection = d3.geo.albers()
-                            .center([0, 55.4])
+                            .center([0, 58])
                             .parallels([50, 60])
-                            .scale(1250)
-                            .translate([width / 2, height / 2]),
+                            .scale(2300)
+                            .translate([width / 1.1, height / 2]),
             path = d3.geo.path().projection(projection);
+
+        path.pointRadius(2);
 
         // DRAW MAP
         svg.append("path")
@@ -25,10 +28,31 @@ $(function () {
         svg.selectAll(".subunit")
             .data(provinces.features)
             .enter().append("path")
-            .attr("class", function(d) { return "subunit " + d.id; })
+            .attr("class", function(d) { return "province " + d.properties.name.split(' ').join(''); })
             .attr("d", path);
+
+        // ADD PLACES ON MAP
+        svg.append("path")
+            .datum(places)
+            .attr("d", path)
+            .attr("class", "place");
+
+        // LABEL PLACES
+        svg.selectAll(".place-label")
+            .data(places.features)
+            .enter().append("input")
+            .attr("class", "place-button")
+            .attr("type", "button")
+            .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+            .attr("name", function(d) { return d.properties.name; });
+
+        $('.place').on('click', function (e) {
+            alert("You clicked a province.");
+        });
     });
 });
+
+
 
 // CLICK ABOUT BUTTON EVENT
 

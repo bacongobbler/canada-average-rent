84ffdfc675699b367ec061054f25a6cfc951f98d
Chocolancer <jasonbolanos92@gmail.com>
2014-03-01 09:38:07 -0800

    Merged changes.

--- www/js/site.js
+++ www/js/site.js
@@ -1,19 +1,19 @@
 
 // RENDER MAP BY DEFAULT DIMENSIONS
 $(function () {
-    var width = $(window).width(),
-        height = $(window).height(),
+    var width = 800,
+        height = 600,
         svg = d3.select("body").append("svg")
                 .attr("width", width)
                 .attr("height", height);
     
-    d3.json("data/canadamap.json", function(error, can) {
+    d3.json("data/canada.json", function(error, can) {
         var subunits = topojson.feature(can, can.objects.subunits),
-            projection = d3.geo.albers()
-                            .center([0, 55.4])
-                            .parallels([50, 60])
-                            .scale(1250)
-                            .translate([width / 2, height / 2]),
+            projection = d3.geo.azimuthalEqualArea()
+                            .rotate([100, -45])
+                            .center([5, 20])
+                            .scale(800)
+                            .translate([w/2, h/2]),
             path = d3.geo.path().projection(projection);
 
         svg.append("path")

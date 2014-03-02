4d4e4e7aa59fbd9d9cf2a973a924b6b750f86d62
Matthew Fisher <matthewf@opdemand.com>
2014-03-02 12:20:06 -0800

    update colours when filters are applied

--- www/js/site.js
+++ www/js/site.js
@@ -67,6 +67,11 @@ function value_exists(element_id, value) {
     return exists;
 }
 
+function on_change() {
+    update_dataset()
+    update_colors()
+}
+
 // update the filtered dataset based on certain actions
 function update_dataset(){
     // set reference to select elements
@@ -83,6 +88,52 @@ function update_dataset(){
         });
     }
 };
+
+// update the colour for each circle
+function update_colors() {
+    var circles = $('circle');
+    var high_value = get_high_value(filtered_dataset);
+    var low_value = get_low_value(filtered_dataset);
+    var hax_value = "#0F0";
+    $.each(circles, function(k, v) {
+        var dataset = dataset_for_city(v.attributes.name.value);
+        var scale = get_scale(parseInt(dataset.average_price), high_value, low_value);
+        hex_value = get_between_colour_by_percent(
+            scale,
+            0x0F0,
+            0xF00)
+            .toString(16)
+            .toUpperCase();
+        hex_value = sprintf("%03s", hex_value);
+        v.setAttribute('style', 'fill: ' + hex_value);
+        v.setAttribute('r', 10 * scale + 3);
+    });
+}
+
+function get_high_value(dataset) {
+    var value = 0;
+    $.each(dataset, function(k, v) {
+        if(parseInt(v.price) > value) {
+            value = parseInt(v.price);
+        }
+    });
+    return value;
+}
+
+function get_low_value(dataset) {
+    var value = 10000000000;
+    $.each(dataset, function(k, v) {
+        if(parseInt(v.price) < value) {
+            value = parseInt(v.price);
+        }
+    });
+    return value;
+}
+
+// gets back a value based on the scale
+function get_scale(value, high_value, low_value) {
+    return ((value - low_value) / (high_value - low_value));
+}
 
 function dataset_for_city(city_name){
     var city_dataset = {};
@@ -108,7 +159,7 @@ function dataset_for_city(city_name){
     average_price = average_price.toFixed(2);
 
     if(isNaN(average_price)) {
-        average_price = "no data";
+        average_price = 0;
     }
     if(coords === "") {
         coords = "no data";
@@ -117,12 +168,12 @@ function dataset_for_city(city_name){
         classification = "no data";
     }
 
-    return { "city": city_name, "coordinates": coords, "geographical_classification": classification, "average_price": "$" + average_price };
+    return { "city": city_name, "coordinates": coords, "geographical_classification": classification, "average_price": average_price };
 }
 
 // set events on 
-$('#filter-year').on('change', update_dataset);
-$('#filter-unit').on('change', update_dataset);
+$('#filter-year').on('change', on_change);
+$('#filter-unit').on('change', on_change);
 
 function city_exists_in_dataset(city_name) {
     var retval = false;
@@ -133,6 +184,21 @@ function city_exists_in_dataset(city_name) {
         }
     });
     return retval;
+}
+
+// finds a colour value between two hex values based on a range of 0-1
+function get_between_colour_by_percent(value, high_color, low_color) {
+    if(typeof(high_color)==='undefined') high_color = 0xFFF;
+    if(typeof(low_color)==='undefined') low_color = 0x000;
+    var r = high_color >> 16;
+    var g = high_color >> 8 & 0xFF;
+    var b = high_color & 0xFF;
+
+    r += ((low_color >> 16) - r) * value;
+    g += ((low_color >> 8 & 0xFF) - g) * value;
+    b += ((low_color & 0xFF) - b) * value;
+
+    return (r << 16 | g << 8 | b);
 }
 
 function draw_map() {
@@ -231,4 +297,190 @@ function add_to_localstorage (data) {
     catch (e) {
         alert("It doesn't look like this browser supports local storage. This application requires local storage to store compare data.");
     }
+}
+
+function sprintf() {
+  //  discuss at: http://phpjs.org/functions/sprintf/
+  // original by: Ash Searle (http://hexmen.com/blog/)
+  // improved by: Michael White (http://getsprink.com)
+  // improved by: Jack
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Dj
+  // improved by: Allidylls
+  //    input by: Paulo Freitas
+  //    input by: Brett Zamir (http://brett-zamir.me)
+  //   example 1: sprintf("%01.2f", 123.1);
+  //   returns 1: 123.10
+  //   example 2: sprintf("[%10s]", 'monkey');
+  //   returns 2: '[    monkey]'
+  //   example 3: sprintf("[%'#10s]", 'monkey');
+  //   returns 3: '[####monkey]'
+  //   example 4: sprintf("%d", 123456789012345);
+  //   returns 4: '123456789012345'
+
+  var regex = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuideEfFgG])/g;
+  var a = arguments;
+  var i = 0;
+  var format = a[i++];
+
+  // pad()
+  var pad = function(str, len, chr, leftJustify) {
+    if (!chr) {
+      chr = ' ';
+    }
+    var padding = (str.length >= len) ? '' : new Array(1 + len - str.length >>> 0)
+      .join(chr);
+    return leftJustify ? str + padding : padding + str;
+  };
+
+  // justify()
+  var justify = function(value, prefix, leftJustify, minWidth, zeroPad, customPadChar) {
+    var diff = minWidth - value.length;
+    if (diff > 0) {
+      if (leftJustify || !zeroPad) {
+        value = pad(value, minWidth, customPadChar, leftJustify);
+      } else {
+        value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
+      }
+    }
+    return value;
+  };
+
+  // formatBaseX()
+  var formatBaseX = function(value, base, prefix, leftJustify, minWidth, precision, zeroPad) {
+    // Note: casts negative numbers to positive ones
+    var number = value >>> 0;
+    prefix = prefix && number && {
+      '2': '0b',
+      '8': '0',
+      '16': '0x'
+    }[base] || '';
+    value = prefix + pad(number.toString(base), precision || 0, '0', false);
+    return justify(value, prefix, leftJustify, minWidth, zeroPad);
+  };
+
+  // formatString()
+  var formatString = function(value, leftJustify, minWidth, precision, zeroPad, customPadChar) {
+    if (precision != null) {
+      value = value.slice(0, precision);
+    }
+    return justify(value, '', leftJustify, minWidth, zeroPad, customPadChar);
+  };
+
+  // doFormat()
+  var doFormat = function(substring, valueIndex, flags, minWidth, _, precision, type) {
+    var number, prefix, method, textTransform, value;
+
+    if (substring === '%%') {
+      return '%';
+    }
+
+    // parse flags
+    var leftJustify = false;
+    var positivePrefix = '';
+    var zeroPad = false;
+    var prefixBaseX = false;
+    var customPadChar = ' ';
+    var flagsl = flags.length;
+    for (var j = 0; flags && j < flagsl; j++) {
+      switch (flags.charAt(j)) {
+        case ' ':
+          positivePrefix = ' ';
+          break;
+        case '+':
+          positivePrefix = '+';
+          break;
+        case '-':
+          leftJustify = true;
+          break;
+        case "'":
+          customPadChar = flags.charAt(j + 1);
+          break;
+        case '0':
+          zeroPad = true;
+          break;
+        case '#':
+          prefixBaseX = true;
+          break;
+      }
+    }
+
+    // parameters may be null, undefined, empty-string or real valued
+    // we want to ignore null, undefined and empty-string values
+    if (!minWidth) {
+      minWidth = 0;
+    } else if (minWidth === '*') {
+      minWidth = +a[i++];
+    } else if (minWidth.charAt(0) == '*') {
+      minWidth = +a[minWidth.slice(1, -1)];
+    } else {
+      minWidth = +minWidth;
+    }
+
+    // Note: undocumented perl feature:
+    if (minWidth < 0) {
+      minWidth = -minWidth;
+      leftJustify = true;
+    }
+
+    if (!isFinite(minWidth)) {
+      throw new Error('sprintf: (minimum-)width must be finite');
+    }
+
+    if (!precision) {
+      precision = 'fFeE'.indexOf(type) > -1 ? 6 : (type === 'd') ? 0 : undefined;
+    } else if (precision === '*') {
+      precision = +a[i++];
+    } else if (precision.charAt(0) == '*') {
+      precision = +a[precision.slice(1, -1)];
+    } else {
+      precision = +precision;
+    }
+
+    // grab value using valueIndex if required?
+    value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
+
+    switch (type) {
+      case 's':
+        return formatString(String(value), leftJustify, minWidth, precision, zeroPad, customPadChar);
+      case 'c':
+        return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
+      case 'b':
+        return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+      case 'o':
+        return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+      case 'x':
+        return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+      case 'X':
+        return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad)
+          .toUpperCase();
+      case 'u':
+        return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+      case 'i':
+      case 'd':
+        number = +value || 0;
+        number = Math.round(number - number % 1); // Plain Math.round doesn't just truncate
+        prefix = number < 0 ? '-' : positivePrefix;
+        value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+        return justify(value, prefix, leftJustify, minWidth, zeroPad);
+      case 'e':
+      case 'E':
+      case 'f': // Should handle locales (as per setlocale)
+      case 'F':
+      case 'g':
+      case 'G':
+        number = +value;
+        prefix = number < 0 ? '-' : positivePrefix;
+        method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+        textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
+        value = prefix + Math.abs(number)[method](precision);
+        return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+      default:
+        return substring;
+    }
+  };
+
+  return format.replace(regex, doFormat);
 }
